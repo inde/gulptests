45eab67bd02ec23b299e46d006a06fec6e0a3322
Andreas <hello@inderesting.com>
2015-02-08 02:16:42 +0200

    'gulptests'

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,23 @@ var publicDir = './public/';
 
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	cssmin = require('gulp-cssmin');
+	cssmin = require('gulp-cssmin'),
+	plumber = require('gulp-plumber');
 
 // Scripts Task
 // Uglifies
 gulp.task('scripts', function(){ // scripts command
 	gulp.src( buildDir + 'js/*.js')
-	.pipe( uglify() )
-	.pipe( gulp.dest( publicDir + '/js') );
+		.pipe( plumber() )/// plumber before
+		.pipe( uglify() )
+		.pipe( gulp.dest( publicDir + '/js') );
 });
 
 gulp.task('styles', function(){ // scripts command
 	gulp.src( buildDir + 'css/*.css')
-    .pipe(cssmin())
-    .pipe(gulp.dest( publicDir + '/css'));
+		.pipe( plumber() )/// plumber before
+		.pipe(cssmin())
+		.pipe(gulp.dest( publicDir + '/css'));
 });
 
 // Watch Task
